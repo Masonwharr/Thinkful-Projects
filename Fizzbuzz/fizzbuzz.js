6976d46fad35058b0d5adb0348cfcf4e708602c9
Masonwharr <mason.harr1@aol.com>
2016-11-14 13:24:15 -0500

    Add files via upload

---
+++ Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,19 @@
+for (i=1; i<=20; i++) {
+if (i % 3 === 0){
+
+if (i % 5 === 0){
+	console.log('fizzbuzz');
+	} else {
+		console.log('fizz');
+	}
+}
+
+else if (i % 5 === 0){
+	console.log('buzz');
+}
+else{
+	console.log(i);
+}
+
+
+}
